98c7f99cff42daf8985294ff0820a427c8c5c6dc
Jaybie <114408008+jaybiemalone@users.noreply.github.com>
2025-03-12 22:57:19 -0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movaflex</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+
+
+    <div class="logo">
+        <img src="asset/MOVAFLEX2.png" alt="MOVAFLEX Image" width="200">
+    </div>
+
+
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li>
+                <a href="product.php">Product ▼</a>
+                    <ul class="dropdown">
+                        <li><a href="#">Office Chairs</a></li>
+                        <li><a href="#">Office Tables and Desks</a></li>
+                        <li><a href="#">ESD</a></li>
+                        <li><a href="#">Cleanroom</a></li>
+                        <li><a href="#">ESD/Cleanrooms</a></li>
+                    </ul>
+            </li>
+            <li><a href="about">About</a></li>
+            <li><a href="contact.php">Contact Us</a></li>
+        </ul>
+    </nav>
+
+
+    <main>
+        <div class="product-container">
+        </div>
+    </main>
+
+
+</body>
+</html>
